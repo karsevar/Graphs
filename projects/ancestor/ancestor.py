a4ef5c69e9d80c2f274947dc7e95d379a30f962b
masonkarsevar <masonkarsevar@gmail.com>
2020-04-13 13:53:10 -0700

    ancestor.py rough draft algorithm finished and passing all tests

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,17 @@
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 def earliest_ancestor(ancestors, starting_node):
     
     # first create the graph in which the relationships 
@@ -66,9 +79,32 @@ def earliest_ancestor(ancestors, starting_node):
 
     if len(graph[starting_node]) == 0:
         return -1
+    else:
+        route = []
+        q = Queue()
+        q.enqueue([starting_node])
+        visited = set() 
 
+        while q.size() > 0:
+            path = q.dequeue() 
 
+            if path[-1] not in visited:
+                visited.add(path[-1])
+
+                if len(graph[path[-1]]) == 0:
+                    if len(route) < len(path):
+                        route = path 
+                    elif len(route) == len(path):
+                        if path[-1] < route[-1]:
+                            route = path
+
+                else:
+                    for neighbor in graph[path[-1]]:
+                        new_path = list(path)
+                        new_path.append(neighbor) 
+                        q.enqueue(new_path)
+    return route[-1]
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print(earliest_ancestor(test_ancestors, 2))
+print(earliest_ancestor(test_ancestors, 6))
